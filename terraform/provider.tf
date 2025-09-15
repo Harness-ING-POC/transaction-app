fe91a09c89c750e75cb4092ee1d8b46194d72f57
Iustin Serban <iustin-iulian.serban@ing.com>
2025-09-15 16:43:41 +0300

    wip

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,5 +9,5 @@ terraform {
 provider "harness" {  
     endpoint   = "https://workshop.harness.io"  
     account_id = "IFG41DWvSnaRLOVNB2uesg"  
-    platform_api_key    = "pat.IFG41DWvSnaRLOVNB2uesg.68c816c985d7245766437b99.qF9cLTx2HPYKQvu81VXi"  
+    platform_api_key    = "pat.IFG41DWvSnaRLOVNB2uesg.68c817f8365cc96fc3c22585.dfA7EYy4ECpG4DSGJz2z"  
 }
