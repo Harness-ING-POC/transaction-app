de484c6e2bc0b2282367b8201a774360e99a5f18
Iustin Serban <iustin-iulian.serban@ing.com>
2025-09-16 13:44:44 +0300

    fixes

--- terraform/default_environments_and_infra.tf
+++ terraform/default_environments_and_infra.tf
@@ -3,7 +3,7 @@ resource "harness_platform_environment" "onboard_envs" {
   name        = each.value
   identifier  = each.value
   org_id      = var.org_id
-  project_id  = var.project_id
+  project_id  = var.proj_id
   type        = "PreProduction"
   description = "Environment ${each.value} created via Terraform"
 }
@@ -14,7 +14,7 @@ resource "harness_platform_infrastructure" "infra" {
 
   name        = "helm-infrastructure"
   identifier  = "helminfra-${each.value}"
-  env_id      = harness_platform_environment.env[each.key].identifier
+  env_id      = harness_platform_environment.onboard_envs[each.key].identifier
   type        = "KubernetesDirect"
   deployment_type = "NativeHelm"
 
